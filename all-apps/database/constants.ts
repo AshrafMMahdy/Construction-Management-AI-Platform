75878834257977ab8df9086d5dd90ad14d0af935
AshrafMMahdy <142499549+AshrafMMahdy@users.noreply.github.com>
2025-08-07 22:22:42 +0300

    Update constants.ts

--- all-apps/database/constants.ts
+++ all-apps/database/constants.ts
@@ -1,6 +1,6 @@
 
 export const APP_URLS = {
-  baselineScheduler: 'https://aischeduler-alpha.vercel.app',
+  baselineScheduler: 'https://construction-management-ai-platform-sandy.vercel.app/',
   contractsAnalysis: 'https://aicontracts-sandy.vercel.app',
   delayAnalysis: 'https://aidelay.vercel.app'
 };
