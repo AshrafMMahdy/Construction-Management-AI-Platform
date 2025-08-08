d8242c18aba98c5c4bdc02eea5c60977eeaf2297
AshrafMMahdy <142499549+AshrafMMahdy@users.noreply.github.com>
2025-08-08 21:38:48 +0300

    Delete all-apps/platform directory

--- all-apps/platform/constants.ts
+++
@@ -1,6 +0,0 @@
-
-export const APP_URLS = {
-  baselineScheduler: 'https://construction-management-ai-platform-sandy.vercel.app/',
-  contractsAnalysis: 'https://aicontracts-sandy.vercel.app',
-  delayAnalysis: 'https://aidelay.vercel.app'
-};
